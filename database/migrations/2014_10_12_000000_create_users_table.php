3f56f80e41a4e16cdbbbb3a860295b4262285680
GetAJobOneDay <titisan@web1.dara.ac.th>
2020-11-06 16:08:55 +0700

    try

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('approve')->default(false);
+		    $table->String('role')->default('student');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('name');
+            $table->string('gender')->default('none');
+            $table->string('student_phone')->default('none');
+            $table->string('birthdate')->default('none');
+            $table->float('gpa')->default(0.00);
+            $table->string('student_img')->default('../assets/unknown.png');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
